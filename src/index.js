2256122ce7efb452c7d579dd6d27ef53a7f6456d
Chu_Tuan_Kiet <abibihkt@gmail.com>
2025-07-10 10:47:42 +0700

    Template_Engine/library_handlebars

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const handlebars = require('express-handlebars');
+const morgan = require('morgan')
+const app = express()
+const port = 8080
+
+//template engine
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
+//HTTP logger
+app.use(morgan('combined'))
+
+
+//route
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/new', (req, res) => {
+  res.render('new');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
