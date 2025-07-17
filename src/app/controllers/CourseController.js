bd985660e64dd83cc3ecbc3deb6dbfea045bf1ca
Chu_Tuan_Kiet <abibihkt@gmail.com>
2025-07-17 17:26:51 +0700

    [CRUD]_Delete_course|Tạo _chức_năng_xóa_trong_trang

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -47,7 +47,13 @@ class CourseController {
         Course.updateOne({ _id: req.params.id}, req.body)
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
-    }    
+    }  
+    //[DELETE]/courses/:id
+    destroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -5,6 +5,7 @@ async function connect() {
         await mongoose.connect('mongodb://localhost:27017/f8_education_dev', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            //seCreateIndex: true,
         });
         console.log('Connect successfully to MongoDB');
     } catch (error) {
