c79feec89e5e569503cf75dcf4421279536269f7
Chu_Tuan_Kiet <abibihkt@gmail.com>
2025-07-20 17:06:58 +0700

    Select all with checkbox | Chức năng chọn & bỏ chọn

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,15 +4,35 @@ const { mutipleMongooseToObject } = require('../../utill/mongoose');
 class MeController {
     //get /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then((courses) =>
+
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) =>
                 res.render('me/stored-courses', {
+                    deletedCount,
                     courses: mutipleMongooseToObject(courses),
-                }),
+                })
             )
             .catch(next);
+
+        // Course.countDocumentsDeleted()
+        //     .then((deletedCount) => {
+        //         console.log(deletedCount);
+        //     })
+
+
+        // Course.find({})
+        //     .then((courses) =>
+        //         res.render('me/stored-courses', {
+        //             courses: mutipleMongooseToObject(courses),
+        //         }),
+        //     )
+        //     .catch(next);
     }
+
+
+
     //get /me/trash/courses
+
     // trashCourses(req, res, next) {
     //     console.log('>>> has findDeleted method?', typeof Course.findDeleted);
     //     Course.findDeleted({ deletedAt: { $ne: null }  })
